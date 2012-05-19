e0daa3ffc21af691d6c56c9b2a96d519b6c2b5b9
Björn Schmitz <profbulb@googlemail.com>
2012-05-19 20:27:44 +0200

    - JDateTimeField updated. - POM updated.

--- src/main/java/com/googlecode/commons/swing/component/datetime/JDateTimeField.java
+++ src/main/java/com/googlecode/commons/swing/component/datetime/JDateTimeField.java
@@ -2,14 +2,13 @@ package com.googlecode.commons.swing.component.datetime;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 import javax.swing.JFormattedTextField;
 import javax.swing.JPopupMenu;
@@ -20,63 +19,33 @@ public class JDateTimeField extends JFormattedTextField {
 
     private static final long serialVersionUID = 1787915955297704574L;
 
-//    private JButton arrowButton;
-    
     private JPopupMenu popup;
     private MiniDateCalendar calendar;
 
     public JDateTimeField() {
         super(new SimpleDateFormat("dd.MM.yyyy"));
-//
-//        setLayout(new MyLayoutManager());
-//        arrowButton = new BasicArrowButton(BasicArrowButton.SOUTH, UIManager.getColor("ComboBox.buttonBackground"), UIManager.getColor("ComboBox.buttonShadow"), UIManager.getColor("ComboBox.buttonDarkShadow"), UIManager.getColor("ComboBox.buttonHighlight"));
-//        arrowButton.setName("ComboBox.arrowButton");
-//        add(arrowButton);
         
         popup = new JPopupMenu();
         popup.setFocusable(false);
         calendar = new MiniDateCalendar();
         calendar.setFocusable(false);
         calendar.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//popup.setVisible(false);
 				JDateTimeField.this.setValue(calendar.getValue());
 			}
 		});
         popup.add(calendar);
         
-        addFocusListener(new FocusListener() {
-			
-			@Override
-			public void focusLost(FocusEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
+        addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent e) {
 				onFocus();
 			}
 		});
-        addKeyListener(new KeyListener() {
-			
-			@Override
-			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
+        addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
 				if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
 					popup.setVisible(false);
 				} else {
@@ -86,7 +55,6 @@ public class JDateTimeField extends JFormattedTextField {
 		});
         
         addCaretListener(new CaretListener() {
-			
 			@Override
 			public void caretUpdate(CaretEvent e) {
 				onFocus();				
@@ -96,13 +64,14 @@ public class JDateTimeField extends JFormattedTextField {
     }
     
     protected void onFocus() {
-		popup.show(JDateTimeField.this, 0, 18);
-		JDateTimeField.this.requestFocus();
+    	if (this.isShowing()) {
+			popup.show(JDateTimeField.this, 0, 18);
+			JDateTimeField.this.requestFocus();
+    	}
     }
 
 	@Override
 	public void commitEdit() throws ParseException {
-		// TODO Auto-generated method stub
 		super.commitEdit();
 		
 		if (isValid()) {
